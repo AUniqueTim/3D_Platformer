f6ccff0464225aecb16cf31454b4348d5fdb77fd
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2020-11-04 03:42:28 -0500

    Added bullet trap, finished TDD.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,41 +4,66 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform bulletSpawn;
-    [SerializeField] private Transform bulletCloneParent;
-    [SerializeField] private Rigidbody bullet;
-    [SerializeField] private int maxBullets;
-    [SerializeField] private int bulletSpeed;
+    //[SerializeField] private Transform player;
+    //[SerializeField] private Transform bulletSpawn;
+    //[SerializeField] private Transform bulletCloneParent;
+    //[SerializeField] private Rigidbody bullet;
+    //[SerializeField] private int maxBullets;
+    //[SerializeField] private int bulletSpeed;
 
-    bool bulletInstantiated;
 
-    public void Awake()
+    //bool bulletInstantiated;
+
+    //public void Awake()
+    //{
+    //    maxBullets = 5;
+    //}
+    //public void Update()
+    //{
+    //    Vector3 forwardDirection = Vector3.forward;
+
+
+    //    if (Input.GetButtonDown("Fire1") && !bulletInstantiated)
+    //    {
+    //        Rigidbody bulletClone = Instantiate(bullet, player.position, player.rotation, bulletCloneParent);
+    //        bulletClone.AddForce(Vector3.forward.normalized * bulletSpeed * Time.deltaTime); //particle effect. use raycast from walls?
+    //        Debug.Log("Fired bullet.");
+    //        bulletInstantiated = true;
+
+    //        //for (int i = 0; i < maxBullets; i++)
+    //        //{
+    //        //    Rigidbody bulletRBClone = Instantiate(bullet, bulletSpawn.position, transform.rotation, bulletCloneParent);
+    //        //    bulletRBClone.AddForce(Vector3.forward.normalized * bulletSpeed);
+    //        //}
+    //    }
+    //    else { bulletInstantiated = false; }
+
+
+
+    //}
+    //private void OnCollisionEnter()
+    //{
+    //    Destroy(gameObject);
+    //    //particle effect.
+    //}
+
+    public void OnCollisionEnter(Collision collision)
     {
-        maxBullets = 5;
-    }
-    public void Update()
-    {
-        if (Input.GetButtonDown("Fire1") && !bulletInstantiated)
+        if (collision.gameObject.tag == "Player")
         {
-            Rigidbody bulletClone = Instantiate(bullet, player.position, player.rotation, bulletCloneParent);
-            bulletClone.AddForce(Vector3.forward.normalized * bulletSpeed * Time.deltaTime);
-            Debug.Log("Fired bullet.");
-            bulletInstantiated = true;
-
-            //for (int i = 0; i < maxBullets; i++)
-            //{
-            //    Rigidbody bulletRBClone = Instantiate(bullet, bulletSpawn.position, transform.rotation, bulletCloneParent);
-            //    bulletRBClone.AddForce(Vector3.forward.normalized * bulletSpeed);
-            //}
+            //lose health, particle effect.
+            Debug.Log("Bullet hit Player.");
         }
-        else { bulletInstantiated = false; }
+        else if (collision.gameObject.tag == "WacMan")
+        {
+            //Spawn new WacMan
+            Destroy(gameObject); //particle effect.
+            Debug.Log("Bullet hit WacMan.");
+        }
+        else if (collision.gameObject)
+        {
+            Destroy(gameObject); //Particle Effect.
+        }
 
-      
-
-    }
-    private void OnCollisionEnter()
-    {
-        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/BulletTrap.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrap : MonoBehaviour
+{
+    [SerializeField] private Transform bulletFirePoint;
+    [SerializeField] private Transform bulletCloneParent;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private Rigidbody bullet;
+    [SerializeField] private Rigidbody bulletClone;
+    [SerializeField] private int bulletSpeed;
+    [SerializeField] private bool bulletInstantiated;
+    Vector3 forwardDirection = Vector3.forward;
+
+
+    private void Awake()
+    {
+        bulletInstantiated = false;
+    }
+    private void Update()
+    {
+        //if (bulletInstantiated == true) {
+        //    bulletClone = Instantiate(bullet/*, bulletCloneParent, instantiateInWorldSpace: true*/);
+        //    bulletClone.transform.Translate(forwardDirection.normalized * bulletSpeed * Time.deltaTime);
+        //}
+        
+
+
+        Debug.DrawRay(transform.position, forwardDirection * maxDistance, Color.red);
+
+        //bulletClone.AddForce(forwardDirection.normalized * bulletSpeed * Time.deltaTime);
+
+        
+
+        //for (int i = 0; i < maxBullets; i++)
+        //{
+        //    Rigidbody bulletRBClone = Instantiate(bullet, bulletSpawn.position, transform.rotation, bulletCloneParent);
+        //    bulletRBClone.AddForce(Vector3.forward.normalized * bulletSpeed);
+        //}
+    }
+    public void FixedUpdate()
+    {
+        
+
+        Ray bulletRay = new Ray(transform.position, forwardDirection);
+
+        RaycastHit bulletHit;
+
+        if (Physics.Raycast(bulletRay, out bulletHit, maxDistance) && bulletInstantiated == false)
+        {
+            if (bulletHit.collider.gameObject.name == "Player" || bulletHit.collider.gameObject.name == "WacMan")
+            {
+
+                bulletClone = Instantiate(bullet/*, bulletFirePoint.position, bullet.rotation*/);
+                bulletClone.MovePosition(forwardDirection.normalized * bulletSpeed * Time.deltaTime); //particle effect. use raycast from walls?
+                bulletInstantiated = true;
+                Debug.Log("Fired bullet.");
+
+            }
+            
+
+        }
+        else if (Physics.Raycast(bulletRay, out bulletHit, maxDistance))
+        {
+            if (bulletHit.collider.gameObject.name!="Player" || bulletHit.collider.gameObject.name != "WacMan" && bulletInstantiated == true)
+            {
+                bulletInstantiated = false;            }
+        }
+       
+        
+    }
+}
+
+
